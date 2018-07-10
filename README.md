34e11a6dc7958ca33c9ae8884cf50e356083ed7b
renie <renie.webdev@gmail.com>
2018-07-10 12:26:17 -0300

    Init project.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Events Project
+
+## Commands
+
+### Install dependencies
+
+`npm i`
+
+### Run app on dev mode
+
+`npm start`
+
+### Run tests
+
+`npm test`
+
+### Builds app for production
+
+`npm run build`
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Dev Bootcamp :: Events!</h1>
+        </header>
+        <p className="App-intro">
+          Several events sources. A single place to check.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
