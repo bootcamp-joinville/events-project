330419d380772a18748b6a34ea67decc680f80bf
Renie Siqueira <renie.webdev@gmail.com>
2018-07-11 13:02:20 -0300

    Adding Bootstrap lib.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'jquery'; // Bootstrap peerdependency (https://getbootstrap.com/docs/4.1/getting-started/webpack/#importing-javascript)
+import 'popper.js'; // Bootstrap peerdependency (https://getbootstrap.com/docs/4.1/getting-started/webpack/#importing-javascript)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
